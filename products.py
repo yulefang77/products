4b7c535bd6288b8388093a0d20c7c9d4d341c38d
yule <yulefang77@gmail.com>
2024-04-06 10:35:17 +0800

    add a function with reading a file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# read a file with name 'z-products.csv'
 products = []
+with open('z-products.csv', 'r', encoding='utf=8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # skip this time, not break this loop
+		name, price = line.strip('\n').split(',')
+		products.append([name, price])
+print(products)
+
+# add product's name and price
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -8,12 +18,14 @@ while True:
 	# p.append(name); p.append(price)
 	# p = [name, price]
 	products.append([name, price])
-print(products)
 
+#　print product's content
+print(products)
 for p in products:
 	print(p[0], '的價格是', p[1], '。')
 
-with open('products.csv', 'w', encoding='utf=8') as f:
+# write a file with name 'z-products.csv'
+with open('z-products.csv', 'w', encoding='utf=8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
